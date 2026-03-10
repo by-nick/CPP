de2c9ab6bdd0c5e4cba429e7ca3580ade002a43f
Nick <laribrito78@gmail.com>
2026-03-10 14:12:42 -0300

    Declarações múltiplas de variáveis, Constantes

---
+++ aula005.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+#define pi 3.1415
+#define dev cout << "Nick B.\n\n";
+int main (){
+	
+	int vidas = 5, tiros = 500, life = 100;
+	
+	cout << pi << "\n\n";
+	dev
+	
+	system("pause");
+	return 0;
+}
