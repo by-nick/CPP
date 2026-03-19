c578443337dfd9b8fc6bb5a05e35d4b5513b20e3
Nick <laribrito78@gmail.com>
2026-03-18 21:52:24 -0300

    Invertendo valores de variáveis

---
+++ aula008.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	
+	int num = 10;
+	
+	cout << num << "\n\n";
+	
+	//num *=-1;
+	num =-num; //os dois jeitos estao certos
+	
+	cout << num << "\n\n";
+	
+	system("pause");
+	return 0;
+}
