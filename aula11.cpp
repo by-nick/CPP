77e39c292eef7d3203f9620793c9750af2cb01d5
by-nick <laribrito78@gmail.com>
2026-04-06 16:42:10 -0300

    and, or e not

---
+++ aula11.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main (){
+
+	int num;
+	
+	num = 8;
+	
+	if (num > 4 && num < 7){
+		cout<<"\nvalor aceito\n";
+		
+	}else{
+		cout<<"\n Valor nao aceito\n";
+	}
+	
+	
+	return 0;
+}
